b6d6016ae2da95a34a656501e0a1f2f3d23fa753
mar215 <sanmar215@gmail.com>
2015-02-06 10:00:00 +0100

    tercer commit

--- BaconFrito/src/bacon/frito/controlador/ControladorIds.java
+++ BaconFrito/src/bacon/frito/controlador/ControladorIds.java
@@ -1,5 +1,27 @@
 package bacon.frito.controlador;
 
+
+//Esto es provisional, mejor tener un sequence en sql
 public class ControladorIds {
+	
+	private int idUsuario;
+	private int idMensaje;
+	
+	private static ControladorIds instancia;
+	
+	static{
+		instancia = new ControladorIds();
+	}
+	
+	private ControladorIds(){
+		//aquí habría que conectar a la base de datos y pedir los últimos ids
+		this.idUsuario = 0;
+		this.idMensaje = 0;
+		
+	}
+	
+	public static ControladorIds getInstance(){
+		return instancia;
+	}
 
 }
