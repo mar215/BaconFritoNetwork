77560353c29c270747d3a72e97468e90dbd29d8d
Paco <chazalmeria@gmail.com>
2015-02-09 16:41:06 +0100

    actualizacion usuario premium

--- BaconFrito/src/bacon/frito/modelo/UsuarioPremium.java
+++ BaconFrito/src/bacon/frito/modelo/UsuarioPremium.java
@@ -3,8 +3,8 @@ package bacon.frito.modelo;
 public class UsuarioPremium extends UsuarioBacon{
 
 	public UsuarioPremium(int id, String user, String pass, String nombre,
-			String apellidos, String telefono, String foto) {
-		super(id, user, pass, nombre, apellidos, telefono, foto);
+			String apellidos, String telefono, String sexo, String bday, String foto) {
+		super(id, user, pass, nombre, apellidos, telefono, sexo, bday, foto);
 	}
 	
 	public boolean sendGroupMessage(String destiny, String subject, String text){
