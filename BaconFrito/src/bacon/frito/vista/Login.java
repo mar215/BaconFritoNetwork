666cc11b1fb0848dd6ca69f516fe28ef6014af6d
Julian <julian.martinezh91@gmail.com>
2015-02-10 14:15:36 +0100

    Cambios en Login para poder comprobar la contraseña e iniciar sesion

--- BaconFrito/src/bacon/frito/vista/Login.java
+++ BaconFrito/src/bacon/frito/vista/Login.java
@@ -1,11 +1,16 @@
 package bacon.frito.vista;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bacon.frito.db.DbConstructor;
+import bacon.frito.modelo.UsuarioBacon;
 
 /**
  * Servlet implementation class Login
@@ -36,12 +41,31 @@ public class Login extends HttpServlet {
 		// Comprobamos session por si el usuario ha accedido directamente
 		boolean activo = (boolean) request.getSession().getAttribute("activo");
 		if(activo == true && (String) request.getSession().getAttribute("user") != null){
-			response.sendRedirect("pagina_perfil.jsp");
+			response.sendRedirect("PaginaPrincipal.jsp");
 		}
 		
 		// Recibimos los parámetros del login en el POST	
 		String user = request.getParameter("user");
 		String pass = request.getParameter("pass");
+		
+		//Buscamos si el usuario está en la base de datos y si está validamos su password
+		DbConstructor constructorDb = DbConstructor.getInstance();
+		if(constructorDb.buscaUsuario(user)){
+			UsuarioBacon usuario = constructorDb.dameUsuario(user);
+			if(usuario.login(pass)){
+				//Si el usuario pasa el check guardamos su nombre en la sesion y le mandamos a
+				//la página de inicio de la red social
+				HttpSession sesion = request.getSession();
+				sesion.setAttribute("user", user);
+				response.sendRedirect("PaginalPrincipal.jsp");
+			}else{
+				//Si falla le mandamos a bienvenida para que se logee
+				response.sendRedirect("Bienvenida.jsp");
+			}
+		}else{
+			//Si no hay usuario le mandamos a la pagina de bienvenida por si ha metido mal el user
+			response.sendRedirect("Bienvenida.jsp");
+		}
 		
 	}
 
