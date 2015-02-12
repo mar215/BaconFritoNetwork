fd882063eb231ddfd8d240fe3c23f1a7a917b705
Paco <chazalmeria@gmail.com>
2015-02-12 16:13:38 +0100

    chapuzilla grupos

--- BaconFrito/WebContent/PaginaCrearGrupo.jsp
+++ BaconFrito/WebContent/PaginaCrearGrupo.jsp
@@ -1,19 +1,13 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-    
-    <%@page import="bacon.frito.modelo.Grupo" %>
-    
-       
-    <% Grupo grupo = (Grupo)request.getAttribute("user");%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Grupos</title>
+<title>Crear pagina</title>
 <link rel="stylesheet" type="text/css" href="HojaEstilosProyecto.css" >
 </head>
 <body>
-
 <div id="header">
 <div id="imagenheader">
 <img src="Imagenes\fondoheader.jpg" alt="fondo header">
@@ -54,23 +48,27 @@
 </table>
 </div>
 </div>
-
 <center>
 <div id="contenido">
 <center>
-<form action="VistaGrupos" method="GET">
+<h1>Crea tu grupo</h1> 
 
-</tr>
+<form action="RegistroGrupo" method="POST">
+<table style="text-align: center">
+<tr><th>Nombre:<th></tr>
+<tr><td><input type="text" name="nombre" <%if(request.getParameter("nombre")!=null){%> value="<%=request.getAttribute("nombre")%>" <%}%>><td></tr>
+<tr><th>Descripcion:<th></tr>
+<tr><td><textarea name="descripcion" rows="10" cols="40" <%if(request.getParameter("descripcion")!=null){%> value="<%=request.getAttribute("descripcion")%>" <%}%>></textarea><td></tr>
+<tr><th>Imagen:<th></tr>
+<tr><td><input type="text" name="imagen" <%if(request.getParameter("imagen")!=null){%> value="<%=request.getAttribute("imagen")%>" <%}%>><td></tr>
+<tr><th>Maximo integrantes:<th></tr>
+<tr><td><input type="text" name="maximos integrantes" <%if(request.getParameter("maximos integrantes")!=null){%> value="<%=request.getAttribute("maximos integrantes")%>" <%}%>><td></tr>
+
+<tr><td><input type="submit" value="Crear grupo"></td></tr>
+</table>
 </form>
 </center>
 </div>
 </center>
-
-<div align="center">
-<tr><td colspan="2">¿Quieres crear tu propio grupo? </td></tr>
-  <form name="form1" action="PaginaCrearGrupo.jsp" target="_blank">
-    <input type="submit" value="Crea tu grupo">
-  </form>
-</div>
 </body>
 </html>
---
+++ BaconFrito/src/bacon/frito/controlador/RegistroGrupo.java
@@ -0,0 +1,90 @@
+package bacon.frito.controlador;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bacon.frito.db.DbConstructor;
+import bacon.frito.modelo.Grupo;
+import bacon.frito.modelo.UsuarioBacon;
+
+/**
+ * Servlet implementation class RegistroGrupo
+ */
+@WebServlet("/RegistroGrupo")
+public class RegistroGrupo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegistroGrupo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola");
+		DbConstructor contructor = DbConstructor.getInstance();
+		
+		int idGrupo = -1;
+		String nombreGrupo = request.getParameter("nombre");
+		String descripcionGrupo = request.getParameter("descripcion");
+		String imagenGrupo = request.getParameter("imagen");
+		int maxIntGrupo = Integer.parseInt(request.getParameter("maximos integrantes"));
+		String activoGrupo = "true";
+		System.out.println("ey");
+		
+		Grupo grup = new Grupo(idGrupo, nombreGrupo, descripcionGrupo,
+				imagenGrupo, maxIntGrupo, activoGrupo);
+		System.out.println(grup);
+		try {
+			if(contructor.buscaGrupo(nombreGrupo)){
+				System.out.println("ya existe");
+				request.getRequestDispatcher("PaginaCrearGrupo.jsp").forward(request, response);
+				return;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	
+		try {
+			contructor.insertarGrupo(grup);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(grup);
+		response.sendRedirect("PaginaGrupos.jsp");
+		
+	}
+	
+
+}
